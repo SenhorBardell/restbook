d047c7f7de7ad8f20a4f80c9c8f40e638d0d6c3b
John <senhorbardell@gmail.com>
2015-04-09 20:21:06 +0200

    Add filter

--- app/controllers/api/places_controller.rb
+++ app/controllers/api/places_controller.rb
@@ -1,6 +1,10 @@
 class Api::PlacesController < ApplicationController
   def index
-    render json: Place.offset(@offset).limit(@limit)
+    if params.has_key? :flags
+      render json: Place.where('flags @> ARRAY[?]::varchar[]', params[:flags]).offset(@offset).limit(@limit)
+    else
+      render json: Place.offset(@offset).limit(@limit)
+    end
   end
 
   def show
