dde62ef8337758f14067e7bc01ad7b48bcf72a29
John <senhorbardell@gmail.com>
2015-04-21 21:28:29 +0200

    Add amazon url constant

--- app/serializers/place_serializer.rb
+++ app/serializers/place_serializer.rb
@@ -2,6 +2,8 @@ class PlaceSerializer < ActiveModel::Serializer
   attributes :id, :name, :type, :logo, :img, :desc,
              :hours, :flags, :address, :phone
 
+  AMAZON_URL = 'https://s3-eu-west-1.amazonaws.com/elasticbeanstalk-eu-west-1-719074273155/img/'
+
   def type
     object.place_type
   end
@@ -19,5 +21,13 @@ class PlaceSerializer < ActiveModel::Serializer
         type: 'Администратор',
         number: 79211040339
     }
+  end
+
+  def img
+    AMAZON_URL + object.img.to_s
+  end
+
+  def logo
+    AMAZON_URL + object.logo.to_s
   end
 end
