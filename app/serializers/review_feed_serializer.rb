9117c1439250e7c85433e71fca05fd5c56a05ea7
John <senhorbardell@gmail.com>
2015-04-28 11:46:44 +0200

    Review feed serializer

--- app/serializers/review_feed_serializer.rb
+++ app/serializers/review_feed_serializer.rb
@@ -3,11 +3,13 @@ class ReviewFeedSerializer < ActiveModel::Serializer
 
   has_one(:place)
 
+  AMAZON_URL = 'https://s3-eu-west-1.amazonaws.com/elasticbeanstalk-eu-west-1-719074273155/img/'
+
   def place
     {
         id: object.place.id,
         type: object.place.place_type,
-        logo: object.place.logo,
+        logo: AMAZON_URL + object.place.logo,
         name: object.place.name
     }
   end
