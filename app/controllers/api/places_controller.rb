9094e2b0ca4fd19f9bef5eb0cb433e90c54656f2
John <senhorbardell@gmail.com>
2015-04-28 19:16:43 +0200

    Debug flag

--- app/controllers/api/places_controller.rb
+++ app/controllers/api/places_controller.rb
@@ -1,9 +1,13 @@
 class Api::PlacesController < ApplicationController
   def index
     if params.has_key? :flags
-      render json: Place.where('flags @> ARRAY[?]::varchar[]', params[:flags]).offset(@offset).limit(@limit)
+      render json: Place.where('flags @> ARRAY[?]::varchar[]', params[:flags])
+                       .pag(@offset, @limit)
+    elsif params.has_key? :debug
+      render json: Place.pag(@offset, @limit)
     else
-      render json: Place.offset(@offset).limit(@limit)
+      render json: Place.where.not('flags @> ARRAY[?]::varchar[]', :hidden)
+                       .pag(@offset, @limit)
     end
   end
 
