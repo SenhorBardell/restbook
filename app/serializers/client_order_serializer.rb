44c4dce4f5f8be80d833011f3e4698b53410e79c
John <senhorbardell@gmail.com>
2015-04-28 22:44:30 +0200

    Client order serializer

--- app/serializers/client_order_serializer.rb
+++ app/serializers/client_order_serializer.rb
@@ -3,12 +3,14 @@ class ClientOrderSerializer < ActiveModel::Serializer
 
   has_one :place
 
+  AMAZON_URL = 'https://s3-eu-west-1.amazonaws.com/elasticbeanstalk-eu-west-1-719074273155/img/'
+
   def place
     {
         id: object.place.id,
         name: object.place.name,
         type: object.place.place_type,
-        logo: object.place.logo
+        logo: AMAZON_URL + object.place.logo
     }
   end
 end
