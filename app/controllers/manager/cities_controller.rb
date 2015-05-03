fd9b57a08ca8b3e464237381a520ce58be1bc2e8
John <senhorbardell@gmail.com>
2015-05-03 21:17:13 +0200

    Manager panel stuff

--- app/controllers/manager/cities_controller.rb
+++ app/controllers/manager/cities_controller.rb
@@ -4,12 +4,45 @@ class Manager::CitiesController < ApplicationController
     @cities = City.all
   end
 
-  def edit
+  def new
+    @city = City.new
+  end
 
+  def create
+    @city = City.new(city_params)
+
+    if @city.save
+      redirect_to manager_cities_url
+    else
+      render 'new'
+    end
+  end
+
+  def update
+    @city = City.find(params[:id])
+
+    if @city.update(city_params)
+      redirect_to manager_cities_url
+    else
+      render 'edit'
+    end
+  end
+
+  def edit
+    @city = City.find(params[:id])
   end
 
   def destroy
+    @city = City.find params[:id]
+    @city.destroy
 
+    redirect_to manager_cities_url
+  end
+
+  private
+
+  def city_params
+    params.require(:city).permit(:name)
   end
 
 end
