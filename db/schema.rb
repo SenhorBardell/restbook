8c2a34590a42c6985f4c8d4e76e13cb3791f5032
John <senhorbardell@gmail.com>
2015-05-26 20:16:19 +0200

    Upload size

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150509193611) do
+ActiveRecord::Schema.define(version: 20150526172011) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -81,6 +81,7 @@ ActiveRecord::Schema.define(version: 20150509193611) do
     t.string  "pass"
     t.string  "login"
     t.integer "city_id"
+    t.string  "phone"
   end
 
   create_table "reviews", force: :cascade do |t|
